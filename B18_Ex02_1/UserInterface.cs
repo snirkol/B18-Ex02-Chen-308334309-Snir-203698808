ac4851283d5402a7985cf68513c508ddfe0f1037
chen lahav <chenlahav6@gmail.com>
2018-04-07 14:21:18 +0300

    add class UserInterface

---
+++ B18_Ex02_1/UserInterface.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B18_Ex02_1
+{
+    static class UserInterface
+    {
+        public static void StartGame(out string o_NameOfPlayer1, out string o_NameOfPlayer2, out int o_SizeOfBoard)
+        {
+            int modeOfGame; // 1-two players 2-computer
+            Console.WriteLine("Hello!\nEnter your name and then press 'Enter'");
+            o_NameOfPlayer1 = Console.ReadLine();
+            while(!validateNameOfPlayer(o_NameOfPlayer1))
+            {
+                Console.WriteLine("The name is not valid, please enter your name and then press 'Enter'");
+                o_NameOfPlayer1 = Console.ReadLine();
+            }
+
+            Console.WriteLine("Enter the size of board (6 or 8 or 10) and then press 'Enter'");
+            o_SizeOfBoard = int.Parse(Console.ReadLine());
+            while (!validateSizeOfBoard(o_SizeOfBoard))
+            {
+                Console.WriteLine("The size is not valid, please enter the size of board (6 or 8 or 10) and then press 'Enter'");
+                o_SizeOfBoard = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("For game with two players press 1\nFor game with the computer press 2");
+            modeOfGame = int.Parse(Console.ReadLine());
+            while(!validateModeOfGame(modeOfGame))
+            {
+                Console.WriteLine("The input is not valid\nFor game with two players press 1\nFor game with the computer press 2");
+                modeOfGame = int.Parse(Console.ReadLine());
+            }
+            if(modeOfGame == 1) //game with two players
+            {
+                Console.WriteLine("Enter the name of player 2 and then press 'Enter'");
+                o_NameOfPlayer2 = Console.ReadLine();
+                while (!validateNameOfPlayer(o_NameOfPlayer2))
+                {
+                    Console.WriteLine("The name is not valid, please enter your name and then press 'Enter'");
+                    o_NameOfPlayer2 = Console.ReadLine();
+                }
+            }
+            else //game with the computer 
+            {
+                o_NameOfPlayer2 = "";
+            }
+        }
+
+        private static bool validateNameOfPlayer(string i_Name)
+        {
+            bool isValidName = true;
+            if((i_Name.Length < 1) && (i_Name.Length > 20))
+            {
+                isValidName = false;
+            }
+
+            if((isValidName)&&(i_Name.Contains(' ')))
+            {
+                isValidName = false;
+            }
+
+            return isValidName;
+        }
+
+        private static bool validateSizeOfBoard(int i_SizeOfBoard)
+        {
+            bool isValidSize = true;
+            if (((i_SizeOfBoard != 6) && (i_SizeOfBoard != 8) && (i_SizeOfBoard != 10)))
+            {
+                isValidSize = false;
+            }
+
+            return isValidSize;
+        }
+
+        private static bool validateModeOfGame(int i_ModeOfGeme)
+        {
+            bool isValidMode = true;
+            if((i_ModeOfGeme != 1) && (i_ModeOfGeme != 2))
+            {
+                isValidMode = false;
+            }
+
+            return isValidMode;
+        }
+    }
+}
