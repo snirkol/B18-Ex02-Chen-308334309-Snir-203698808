a1430e4a2d35cc7e67cb16e8123ed7ccf7be189d
chen lahav <chenlahav6@gmail.com>
2018-04-21 17:01:16 +0300

    add computer class

---
+++ B18_Ex02_1/Computer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B18_Ex02_1
+{
+    static class Computer
+    {
+        public static void GetParametersOfCurrentTurn(string i_CurrentPlayerName, char i_SignOfCurrentPlayer, out int? o_IndexOfCurrentRow,
+              out int? o_IndexOfCurrentCol, out int? o_IndexOfNewRow, out int? o_IndexOfNewCol, Dictionary<Position, List<Position>> i_PossibleMoves)
+        {
+            o_IndexOfCurrentRow = 1;
+            o_IndexOfCurrentCol = 1;
+            o_IndexOfNewRow = 1;
+            o_IndexOfNewCol = 1;
+        }
+    }
+}
