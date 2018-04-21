1039e617738df091c43217e22a58f0d6df2a6cab
chen lahav <chenlahav6@gmail.com>
2018-04-21 17:28:55 +0300

    rand selection of computer

--- B18_Ex02_1/Computer.cs
+++ B18_Ex02_1/Computer.cs
@@ -9,14 +9,20 @@ namespace B18_Ex02_1
     static class Computer
     {
         public static void GetParametersOfCurrentTurn(string i_CurrentPlayerName, char i_SignOfCurrentPlayer, out int? o_IndexOfCurrentRow,
-              out int? o_IndexOfCurrentCol, out int? o_IndexOfNewRow, out int? o_IndexOfNewCol, out bool o_IsQuit, Dictionary<Position, List<Position>> posibleMovesPerPosition)
+              out int? o_IndexOfCurrentCol, out int? o_IndexOfNewRow, out int? o_IndexOfNewCol, out bool o_IsQuit, Dictionary<Position, List<Position>> i_PosibleMoves)
         {
-            Random rand = new Random();
+            
+            Random randKey = new Random();
+            Random randValue = new Random();
 
-            o_IndexOfCurrentRow = 1;
-            o_IndexOfCurrentCol = 1;
-            o_IndexOfNewRow = 1;
-            o_IndexOfNewCol = 1;
+            Position currentRandomKey = i_PosibleMoves.Keys.ToArray()[(int)randKey.Next(0, i_PosibleMoves.Keys.Count - 1)];
+            List<Position> valuesOfRandKey = i_PosibleMoves[currentRandomKey];
+            Position currentRandomValue = valuesOfRandKey[randValue.Next(0, valuesOfRandKey.Count - 1)];
+
+            o_IndexOfCurrentRow = currentRandomKey.m_Row;
+            o_IndexOfCurrentCol = currentRandomKey.m_Col;
+            o_IndexOfNewRow = currentRandomValue.m_Row;
+            o_IndexOfNewCol = currentRandomValue.m_Col;
             o_IsQuit = false;
 
         }
